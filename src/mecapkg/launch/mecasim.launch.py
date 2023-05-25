81fc4ae2b6f68f7a95d1d9ef998bec3ef24598a6
Nayan Banik <129023126+nynbnk@users.noreply.github.com>
2023-05-25 17:05:55 -0600

    first upload files

---
+++ src/mecapkg/launch/mecasim.launch.py
@@ -0,0 +1,41 @@
+import os
+from ament_index_python.packages import get_package_share_directory
+from launch import LaunchDescription
+from launch.actions import IncludeLaunchDescription
+from launch_ros.actions import Node
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+
+
+
+def generate_launch_description():
+    package_name = "mecapkg"
+
+    rsp = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource([os.path.join(
+        get_package_share_directory(package_name), 'launch', 'rsp.launch.py'
+        )]), launch_arguments={'use_sim_time': 'true'}.items()
+    )
+
+    gazebo = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource([os.path.join(
+        get_package_share_directory('gazebo_ros'), 'launch', 'gazebo.launch.py'
+        )])
+    )
+
+    spawn_entity = Node(package='gazebo_ros', executable='spawn_entity.py', arguments=['-topic', 'robot_description', '-entity', 'my_bot'], output='screen')
+
+    vel_cont_spawner = Node(
+        package="controller_manager",
+        executable="spawner",
+        arguments=["vel_cont"],
+    )
+
+    joint_broad_spawner = Node(
+        package="controller_manager",
+        executable="spawner",
+        arguments=["joint_state_broadcaster"],
+)
+        
+
+    return LaunchDescription([rsp, gazebo, spawn_entity, vel_cont_spawner, joint_broad_spawner])
+
